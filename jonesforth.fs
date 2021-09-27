b4a582deb78ac178c798bc98de57df267181d9f2
Daniel Thompson <daniel@redfelineninja.org.uk>
2021-09-27 19:07:32 +0100

    Update license header

--- jonesforth.fs
+++ jonesforth.fs
@@ -1,17 +1,13 @@
-\ -*- text -*-
-\	A sometimes minimal FORTH compiler and tutorial for Linux / i386 systems. -*- asm -*-
+\	SPDX-License-Identifier: MIT
+
+\	A sometimes minimal FORTH compiler and tutorial for any C compiler that
+\       implements the GNU C labels-as-values language extension.
+\
+\	By Daniel Thompson <daniel@redfelineninja.org.uk>
+\	  and based on the fabulous public domain assembler version for Linux / i386
 \	By Richard W.M. Jones <rich@annexia.org> http://annexia.org/forth
-\	This is PUBLIC DOMAIN (see public domain release statement below).
-\	$Id: jonesforth.f,v 1.18 2009-09-11 08:32:33 rich Exp $
 \
-\	The first part of this tutorial is in jonesforth.S.  Get if from http://annexia.org/forth
-\
-\	PUBLIC DOMAIN ----------------------------------------------------------------------
-\
-\	I, the copyright holder of this work, hereby release it into the public domain. This applies worldwide.
-\
-\	In case this is not legally possible, I grant any entity the right to use this work for any purpose,
-\	without any conditions, unless such conditions are required by law.
+\	The first part of this tutorial is in jonesforth.c.
 \
 \	SETTING UP ----------------------------------------------------------------------
 \
